d8483d35782340fa530faebb0ba2df05d5438728
Nils Vogels <n.vogels@aves-it.nl>
2021-03-24 16:50:44 +0100

    Compatibility with terraform 0.13

--- versions.tf
+++ versions.tf
@@ -1,4 +1,15 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "~> 1.25.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.1.0"
+    }
+  }
 }
+
